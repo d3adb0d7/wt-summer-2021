c0ac80dab860ac54c945b1ae6cc5b3c06604a571
d3adb0d7 <56756645+d3adb0d7@users.noreply.github.com>
2021-06-05 12:51:13 +0600

    final upload

---
+++ lab_task_1/registration.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="">
+<table>
+    <tr>
+    <th>
+        <label for="fname">Name:</label>
+    </th>
+    <th>
+         <input type="text" id="name" name="name">
+    </th>
+    </tr>
+    <tr>
+    <th>
+        <label for="email">Email</label>
+    </th>
+    <th>
+        <input type="text" id="email" name="email">
+    </th>
+    </tr>
+    <tr>
+    <th>
+        <label for="password">Password</label>
+    </th>
+    <th>
+        <input type="password" id="password" name="password">
+    </th>
+    </tr>
+    <tr>
+    <th>
+        <label for="comment">Comment</label>
+    </th>
+    <th>
+        <textarea name="comment" id="" cols="30" rows="10"></textarea>
+    </th>
+    </tr>
+    <tr>
+    <th>
+        <label for="fname">Gender:</label>
+    </th>
+    <th>
+        <input type="radio" id="male" name="gender" value="male">
+        <label for="male">Male</label>
+        <input type="radio" id="female" name="gender" value="female">
+        <label for="female">Female</label>
+        <input type="radio" id="other" name="gender" value="other">
+        <label for="other">Other</label>
+    </th>
+    </tr>
+    <tr>
+    <th>
+        <label for="fname">Please select hobby:</label>
+    </th>
+    <th>
+        <label for="Singing">Singing</label>
+        <input type="checkbox" id="hobby" name="hobby" value="Singing">
+        <label for="Dancing">Dancing</label>
+        <input type="checkbox" id="hobby" name="hobby" value="Dancing">
+        <label for="Reading">Reading</label>
+        <input type="checkbox" id="hobby" name="hobby" value="Reading">
+    </th>
+    </tr>
+    <tr>
+    <th>
+        <label for="fname">Please chose files:</label>
+    </th>
+    <th>
+        <input type="file" name="image" id="image" />
+    </th>
+    </tr>
+    <tr>
+    <th>
+        <input type="submit" value="Submit">
+        <input type="reset" value="reset">
+    </th>
+    </tr>
+</table>
+</form>
+</body>
+</html>
+
+<!-- 
+  
+  
+  
+  
+  <br><br>
+  
+   -->
